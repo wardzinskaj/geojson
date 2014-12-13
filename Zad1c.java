9550b0152e362479f8e15e2929a7d1caa3eb008e
Justyna Wardzińska <wardzinskaj@users.noreply.github.com>
2014-12-13 11:59:06 +0100

    Update Zad1c.java

--- Zad1c.java
+++ Zad1c.java
@@ -14,12 +14,12 @@ public class Zad1c {
 
     public static void main(String[] args) throws UnknownHostException {
 
-        int allTags = 0;
-        String objTags[];
-        Map<String, Boolean> uniqueTags = new HashMap<String, Boolean>();
-        long time1,time2;
+        int Tagi = 0;
+        String slowaTagi[];
+        Map<String, Boolean> inneTagi = new HashMap<String, Boolean>();
+        long czas1,czas2;
 
-        time1 = System.currentTimeMillis()/1000;
+        czas1 = System.currentTimeMillis()/1000;
         MongoClient mongoClient = new MongoClient("localhost", 27017);
         DB db = mongoClient.getDB("local");     
         DBCollection coll = db.getCollection("train");
@@ -27,22 +27,22 @@ public class Zad1c {
         DBCursor cursor = coll.find();
         try {
             while(cursor.hasNext()) {
-                BasicDBObject obj = (BasicDBObject)cursor.next();
-                objTags = obj.getString("Tags").split(" ");
-                obj.put("Tags", objTags);
-                coll.save(obj);
+                BasicDBObject dbObj = (BasicDBObject)cursor.next();
+                slowaTagi = dbObj.getString("Tags").split(" ");
+                dbObj.put("Tags", slowaTagi);
+                coll.save(dbObj);
 
-                allTags += objTags.length;
-                for(int i=0;i<objTags.length;i++) {
-                    if(uniqueTags.get(objTags[i]) == null)
-                        uniqueTags.put(objTags[i], true);
+                Tagi += slowaTagi.length;
+                for(int i=0;i<slowaTagi.length;i++) {
+                    if(inneTagi.get(slowaTagi[i]) == null)
+                        inneTagi.put(slowaTagi[i], true);
                 }
             }
         } finally {
-            time2 = System.currentTimeMillis()/1000;
-            System.out.println("Wszystkie tagi: "+allTags);
-            System.out.println("Unikalne tagi: "+uniqueTags.size());
-            System.out.println("Przetwarzanie zajelo "+(time2-time1)/60+"m "+(time2-time1)%60+"s");
+            czas2 = System.currentTimeMillis()/1000;
+            System.out.println("Wszystkie tagi: "+Tagi);
+            System.out.println("Unikalne tagi: "+inneTagi.size());
+            System.out.println("Przetwarzanie zajelo "+(czas2-czas1)/60+"m "+(czas2-czas1)%60+"s");
             cursor.close();
         }
 
